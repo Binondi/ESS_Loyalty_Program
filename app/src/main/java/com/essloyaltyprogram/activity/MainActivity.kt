167b869fae558ee81668482221f9ac65a2fb6bab
Binondi <binondiborthakur56@gmail.com>
2025-02-14 19:36:47 +0530

    made some changes

--- app/src/main/java/com/essloyaltyprogram/activity/MainActivity.kt
+++ app/src/main/java/com/essloyaltyprogram/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.essloyaltyprogram.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -78,7 +79,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.bell -> {
-                Toast.makeText(this, "Notification Clicked", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, NotificationActivity::class.java))
                 true
             }
             else -> super.onOptionsItemSelected(item)
--- app/src/main/java/com/essloyaltyprogram/activity/NotificationActivity.kt
+++ app/src/main/java/com/essloyaltyprogram/activity/NotificationActivity.kt
@@ -1,21 +1,25 @@
 package com.essloyaltyprogram.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.essloyaltyprogram.R
+import com.essloyaltyprogram.databinding.ActivityNotificationBinding
 
 class NotificationActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityNotificationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_notification)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityNotificationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
     }
+
 }
